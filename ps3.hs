071034af1b29f23c10010f8853f629e9bad3f7cf
qianyilun <qianyilun@gmail.com>
2016-11-19 00:13:07 -0800

    Prepare to submit

--- ps3.hs
+++ ps3.hs
@@ -40,20 +40,29 @@ invert_bits2 :: [Int] -> [Int]
 invert_bits2 seq = map flip_bit seq
 
 -- (c)
--- invert_bits3 :: [Int] -> [Int]
--- invert_bits3 seq | length seq == 1 = [flip_bit (head seq)]
---                  | otherwise = [x] ++ xs
---                    where x = flip_bit (head seq)
---                          xs = invert_bits3 [n | n <- (tail seq), True]
-
-
-
--- invert_bits4 :: [Int] -> [Int]
--- invert_bits4 seq = take (length seq) [n | n <- (++) [(flip_bit (head seq))] 
---                                                      (invert_bits4 (tail seq)), True]
-
 invert_bits3 :: [Int] -> [Int]
 invert_bits3 seq = [flip_bit bit | bit <- seq, True]
 
+-- Q5
+bit_count :: [Int] -> (Int, Int)
+bit_count seq = ((zeros seq), (ones seq))
 
+zeros :: [Int] -> Int
+zeros []     = 0
+zeros (x:xs)
+        | x == 0 = 1 + zeros xs 
+        | otherwise = zeros xs
 
+ones :: [Int] -> Int
+ones []     = 0
+ones (x:xs)
+        | x == 1 = 1 + ones xs 
+        | otherwise = ones xs
+
+-- Q6
+all_bit_seqs :: Int -> [[Int]]
+all_bit_seqs n 
+             | n < 1 = []
+             | n == 1 = [[1], [0]]
+             | otherwise = (map ((++) [0]) (all_bit_seqs (n-1))) ++ 
+                           (map ((++) [1]) (all_bit_seqs (n-1)))
