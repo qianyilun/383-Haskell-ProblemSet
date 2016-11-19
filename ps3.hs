190f7ae23c1efb5447a8dd07fb3e49e3d4d753e3
qianyilun <qianyilun@gmail.com>
2016-11-18 23:17:44 -0800

    Left two questions

--- ps3.hs
+++ ps3.hs
@@ -29,12 +29,31 @@ is_bit_seq3 :: [Int] -> Bool
 is_bit_seq3 = all (is_bit)
 
 -- Q4
+-- (a)
 invert_bits1 :: [Int] -> [Int]
-invert_bits1 (x, xs) = (flip_bit x) ++ (invert_bits1 xs)
+invert_bits1 seq 
+                | length seq == 1 = [flip_bit (head seq)]
+                | otherwise = (++) [(flip_bit (head seq))] (invert_bits1 (tail seq))
+
+-- (b)
+invert_bits2 :: [Int] -> [Int]
+invert_bits2 seq = map flip_bit seq
+
+-- (c)
+-- invert_bits3 :: [Int] -> [Int]
+-- invert_bits3 seq | length seq == 1 = [flip_bit (head seq)]
+--                  | otherwise = [x] ++ xs
+--                    where x = flip_bit (head seq)
+--                          xs = invert_bits3 [n | n <- (tail seq), True]
 
 
 
+-- invert_bits4 :: [Int] -> [Int]
+-- invert_bits4 seq = take (length seq) [n | n <- (++) [(flip_bit (head seq))] 
+--                                                      (invert_bits4 (tail seq)), True]
 
+invert_bits3 :: [Int] -> [Int]
+invert_bits3 seq = [flip_bit bit | bit <- seq, True]
 
 
 
